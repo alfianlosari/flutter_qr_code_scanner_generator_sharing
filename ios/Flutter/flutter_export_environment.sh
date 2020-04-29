4d1668c6c9566e84a06e3572cae2f4e23e1d8ab7
Leonardo Almeida de Araújo <37451620+BulletSentence@users.noreply.github.com>
2020-04-29 11:10:23 -0300

    Update on dependencies, Version ,and Pubspec.yaml
    build.gradle version is now 28, and Pubspec updated

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\Almeida\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Almeida\Documents\flutter_qr_code_scanner_generator_sharing"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\Almeida\Documents\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
